f6e1f3e22fec393622e33a3550abb8e2c332ccee
kingslayerrr <itspriiyanshu@gmail.com>
2021-02-11 16:03:06 +0530

    Done

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,s;
+        System.out.print("ENTER TWO NUMBERS: ");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sc.close();
+        s=a+b;
+        System.out.println("SUM OF TWO NUMBERS="+s+"\n\n");
+    }
+}
